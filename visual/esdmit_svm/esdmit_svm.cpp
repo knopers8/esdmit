8e9d596222106a2d7678d778facd3dfa9cf9b29a
knopers8 <knopers8@op.pl>
2016-11-24 12:14:04 +0100

    Created MVSC project with Eigen
    should work everywhere with no additional configuration

---
+++ visual/esdmit_svm/esdmit_svm.cpp
@@ -0,0 +1,19 @@
+// esdmit_svm.cpp : Defines the entry point for the console application.
+//
+
+
+
+#include "stdafx.h"
+#include "Eigen/Eigen"
+
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Eigen::Vector2i vec(72,87);
+	
+	std::cout << (char)vec[0] << "ello " << (char)vec[1] << "orld!!!12!!!@!!!!" << std::endl;
+
+	return 0;
+}
+

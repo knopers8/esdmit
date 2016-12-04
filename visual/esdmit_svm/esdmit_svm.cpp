d9af86acead07fee5bacafec6d70e78ee665b029
knopers8 <knopers8@op.pl>
2016-12-04 23:21:08 +0100

    mvsc - added time measurement for testing optimalization effectiveness

--- visual/esdmit_svm/esdmit_svm.cpp
+++ visual/esdmit_svm/esdmit_svm.cpp
@@ -8,13 +8,23 @@
 #include "BinarySVM.h"
 #include "MultiSVM.h"
 
+//not ours - timer
+#include "util.hpp"
+
 #include <iostream>
+#include <ctime>
 
 #define BINARY_SVM_TESTx
 #define MULTI_SVM_TEST
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	util::Timer timer;
+	double t_start = static_cast<double>(timer.getTimeMicroseconds()) / 1000.0;
+	double t_train;
+	double t_classify;
+	double t_end;
+
 #ifdef BINARY_SVM_TEST
 	Eigen::Matrix<double, 9, 2> teach_data_inputs;
 	Eigen::VectorXi teach_data_outputs(9);
@@ -50,16 +60,22 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	BinarySVM svm_instance;
 
+	t_train = static_cast<double>(timer.getTimeMicroseconds()) / 1000.0;
 	svm_instance.Train(teach_data_inputs, teach_data_outputs, w0, C, max_it, eps);
 	
 
+	t_classify = static_cast<double>(timer.getTimeMicroseconds()) / 1000.0;
 	classify_outputs = svm_instance.Classify(teach_data_inputs);
+
+	t_end = static_cast<double>(timer.getTimeMicroseconds()) / 1000.0;
 	std::cout << classify_outputs << std::endl;
 #endif // BINARY_SVM_TEST
 #ifdef MULTI_SVM_TEST
+
 	Eigen::Matrix<double, 9, 2> teach_data_inputs;
 	Eigen::VectorXi teach_data_outputs(9);
 	Eigen::VectorXi classify_outputs;
+
 
 	teach_data_inputs << 1, 3,
 						2, 2,
@@ -92,14 +108,24 @@ int _tmain(int argc, _TCHAR* argv[])
 	//std::cout << "teach_data_inputs: " << std::endl << teach_data_inputs << std::endl;
 	//std::cout << "norm_data: " << std::endl << norm_data << std::endl;
 
+	t_train = static_cast<double>(timer.getTimeMicroseconds()) / 1000.0;
 	svm_instance.Train(teach_data_inputs, teach_data_outputs, w0, C, max_it, eps);
 
+	t_classify = static_cast<double>(timer.getTimeMicroseconds()) / 1000.0;
 	classify_outputs = svm_instance.Classify(teach_data_inputs);
+
+	t_end = static_cast<double>(timer.getTimeMicroseconds()) / 1000.0;
 
 	std::cout << "teach_data_outputs: " << teach_data_outputs << std::endl;
 	std::cout << "classify_outputs: " << classify_outputs << std::endl;
 
 #endif //MULTI_SVM_TEST
+	
+	
+
+	std::cout << "Program runtime: " << (t_end - t_start) << " ms" << std::endl;
+	std::cout << "Training runtime: " << (t_classify - t_train) << " ms" << std::endl;
+	std::cout << "Classifying runtime: " << (t_end - t_classify) << " ms" << std::endl;
 
 	return 0;
 }

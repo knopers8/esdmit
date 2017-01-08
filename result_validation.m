695e2c6f5659be5eae2c84e4771c204cd9098d73
MichalG <michal.gora997@gmail.com>
2017-01-08 19:01:24 +0100

    results validation 2

--- result_validation.m
+++ result_validation.m
@@ -10,8 +10,22 @@ for i=1:length(folders)
     result = importdata('classified_outputs.txt');
     label  = importdata('correct_outputs.txt');
     good_percentage=100*(sum((result-label)==0) /length(result));
-    heathy_id=round(mean(label));
     fprintf(fileID,'Percentage of good classifications %f\n',good_percentage);
+    
+     healthy_id=mode(label);
+     healthy_result=result== healthy_id;
+     sick_result=result~= healthy_id;
+     healthy_label=label== healthy_id;
+     sick_label=label~= healthy_id;
+     
+     TP =sum(sick_result==sick_label) ;%True positive
+     TN =sum(healthy_result==healthy_label) ;% True negative
+     FP =sum(and(sick_result,healthy_label));  % False positive
+     FN  =sum(and(healthy_result,sick_label)); % False negative
+     sensivity=TP/(TP+FN);
+     specificity=TN/(TN+FP);
+     fprintf(fileID,'Sensivity %f\n',sensivity);
+     fprintf(fileID,'Specificity %f\n',specificity);
     fprintf(fileID,' \n');
 end
 fclose(fileID)
